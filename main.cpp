cf704054fab03ab66d1488ecc4b01ddece34a2c7
rose <viransembi@gmail.com>
2024-05-22 13:52:20 +0100

    fixed val yaw

--- main.cpp
+++ main.cpp
@@ -1,134 +1,182 @@
-#include <algorithm>
 #include <iostream>
-#include <vector>
+#include <algorithm>
 #include <charconv>
+#include <vector>
 #include "includes/colored_cout.h"
-
-const double cm_per_in = 2.54;
 
 struct GameYaw {
     std::string game;
     double yaw;
 };
 
+// game yaw values
 const std::vector<GameYaw> GAME_YAWS = {
-    {"source", 0.022},
-    {"apex", 0.022},
-    {"quake", 0.022},
-    {"cs", 0.022},
-    {"q3a", 0.022},
-    {"bf5", 0.022},
-    {"battlefield5", 0.022},
-    {"d2", 0.0066},
-    {"destiny2", 0.0066},
-    {"ow", 0.0066},
-    {"overwatch", 0.0066},
-    {"fn", 0.5715},
-    {"fortnite", 0.5715},
-    {"ql", 0.02105},
-    {"quakelive", 0.02105},
-    {"pubg", 2.22222},
-    {"ow2", 0.006666},
-    {"overwatch2", 0.006666},
-    {"doom", 0.0439453125},
-    {"qcde", 0.0439453125},
-    {"val", 0.051097900390625},
-    {"valorant", 0.051097900390625},
-    {"siege", 0.00572957795130823},
-    {"r6", 0.00572957795130823},
-    {"reflex", 0.00572957795130823},
-    {"ut", 0.596}
+        {"valorant",        0.07},
+        {"val",             0.07},
+        {"source",          0.022},
+        {"apex",            0.022},
+        {"quake",           0.022},
+        {"cs",              0.022},
+        {"q3a",             0.022},
+        {"bf5",             0.022},
+        {"battlefield5",    0.022},
+        {"ut",              0.596},
+        {"d2",              0.0066},
+        {"destiny2",        0.0066},
+        {"ow",              0.0066},
+        {"overwatch",       0.0066},
+        {"fn",              0.5715},
+        {"fortnite",        0.5715},
+        {"ql",              0.02105},
+        {"quakelive",       0.02105},
+        {"pubg",            2.22222},
+        {"ow2",             0.006666},
+        {"overwatch2",      0.006666},
+        {"doom",            0.0439453125},
+        {"qcde",            0.0439453125},
+        {"siege",           0.00572957795130823},
+        {"r6",              0.00572957795130823},
+        {"reflex",          0.00572957795130823}
 };
 
-// determine game and return the corresponding yaw
-double determine_game( const std::string& game ) {
-    auto identified_game = std::find_if(GAME_YAWS.begin(), GAME_YAWS.end(), [&]( const GameYaw& game_yaw ) { return game_yaw.game == game; });
-    return ( identified_game != GAME_YAWS.end() ) ? identified_game->yaw : 0.0;
-}
+// Use a namespace to avoid potential naming conflicts
+namespace sens_converter {
 
-// calcs
-// returns the cm value : ( ( 360 / ( yaw * sens ) ) / dpi * 2.54 )
-void get_cm( double yaw, double sens, int dpi ) {
-    auto inc = yaw * sens;
-    auto counts_per_rev = 360 / inc;
-    std::cout << " " << sens << " in game = " << clr::green << counts_per_rev / dpi * cm_per_in << clr::reset << " cm/360\n\n ";
-    system( "pause" );
-}
+    const double cm_per_in = 2.54;
 
-// returns the sens value : ( ( 360 / ( cm / 2.54 * dpi ) ) / yaw )
-void get_sens( double yaw, double cm, int dpi ) {
-    auto counts_per_rev = cm / cm_per_in * dpi;
-    auto inc = 360 / counts_per_rev;
-    std::cout << " " << cm << " cm/360 = " << clr::green << inc / yaw << clr::reset << " in game\n ";
-    system( "pause" );
-}
-
-// game sens dpi -> cm/360
-void cm() {
-    std::string game;
-    double input_game{};
-    double input_sens;
-    int input_dpi;
-
-    while ( input_game == 0 ) {
-        std::cout << " enter your desired game\n" << clr::green << "  > " << clr::reset;
-        std::cin  >> game;
-        input_game = determine_game( game );
+    // determine game and return the corresponding yaw
+    double determine_game( const std::string& game ) {
+        auto identified_game = std::find_if( GAME_YAWS.begin(), GAME_YAWS.end(), [&]( const GameYaw& game_yaw ) { return game_yaw.game == game; });
+        return ( identified_game != GAME_YAWS.end() ) ? identified_game->yaw : 0.0;
     }
 
-    std::cout << " enter your sensitivity\n" << clr::green << "  > " << clr::reset;
-    std::cin  >> input_sens;
-    std::cout << " enter your dpi\n" << clr::green << "  > " << clr::reset;
-    std::cin  >> input_dpi;
-
-    get_cm( input_game, input_sens, input_dpi );
-}
-
-// game1 game2 sens -> newsens
-void convert() {
-    std::string game1, game2;
-    double input_game1{}, input_game2{};
-    double input_sens;
-    double conv;
-
-    while ( input_game1 == 0 ) {
-        std::cout << " enter the game you're converting from\n" << clr::green << "  > " << clr::reset;
-        std::cin  >> game1;
-        input_game1 = determine_game( game1 );
-    }
-    while ( input_game2 == 0 ) {
-        std::cout << " enter the game you're converting to\n" << clr::green << "  > " << clr::reset;
-        std::cin  >> game2;
-        input_game2 = determine_game( game2 );
-    }
-    std::cout << " enter your sens\n" << clr::green << "  > " << clr::reset;
-    std::cin  >> input_sens;
-
-    conv = input_sens * input_game1;
-    std::cout << " " << input_sens << " on " << game1 << " = " << clr::green << ( conv / input_game2 ) << clr::reset << " on " << game2 << "\n ";
-    system( "pause" );
-    std::cout << "\n";
-}
-
-// game cm dpi -> sens
-void sens() {
-    std::string game;
-    double input_game{};
-    double input_cm;
-    int input_dpi;
-
-    while ( input_game == 0 ) {
-        std::cout << " enter your desired game\n" << clr::green << "  > " << clr::reset;
-        std::cin  >> game;
-        input_game = determine_game( game );
+    // calcs
+    // returns the cm value : ( ( 360 / ( yaw * sens ) ) / dpi * 2.54 )
+    void get_cm( double yaw, double sens, int dpi ) {
+        auto inc = yaw * sens;
+        auto counts_per_rev = 360 / inc;
+        std::cout << " " << sens << " in game = " << clr::green << counts_per_rev / dpi * cm_per_in << clr::reset << " cm/360\n\n ";
+        system( "pause" );
     }
 
-    std::cout << " enter your cm\n"  << clr::green << "  > " << clr::reset;
-    std::cin  >> input_cm;
-    std::cout << " enter your dpi\n" << clr::green << "  > " << clr::reset;
-    std::cin  >> input_dpi;
+    // returns the sens value : ( ( 360 / ( cm / 2.54 * dpi ) ) / yaw )
+    void get_sens( double yaw, double cm, int dpi ) {
+        auto counts_per_rev = cm / cm_per_in * dpi;
+        auto inc = 360 / counts_per_rev;
+        std::cout << " " << cm << " cm/360 = " << clr::green << inc / yaw << clr::reset << " in game\n ";
+        system( "pause" );
+    }
 
-    get_sens( input_game, input_cm, input_dpi );
+    // game sens dpi -> cm/360
+    void cm() {
+        std::string game;
+        double input_game,
+               input_sens;
+        int input_dpi;
+
+        while ( input_game == 0 ) {
+            std::cout << " enter your desired game\n" << clr::green << "  > " << clr::reset;
+            std::cin >> game;
+            input_game = determine_game( game );
+        }
+
+        std::cout << " enter your sensitivity\n" << clr::green << "  > " << clr::reset;
+        std::cin >> input_sens;
+        std::cout << " enter your dpi\n" << clr::green << "  > " << clr::reset;
+        std::cin >> input_dpi;
+
+        get_cm( input_game, input_sens, input_dpi );
+    }
+
+    // game1 game2 sens -> newsens
+    void convert() {
+        std::string game1, game2;
+        double input_game1, input_game2,
+               input_sens,
+               conv;
+
+        while ( input_game1 == 0 ) {
+            std::cout << " enter the game you're converting from\n" << clr::green << "  > " << clr::reset;
+            std::cin >> game1;
+            input_game1 = determine_game( game1 );
+        }
+        while ( input_game2 == 0 ) {
+            std::cout << " enter the game you're converting to\n" << clr::green << "  > " << clr::reset;
+            std::cin >> game2;
+            input_game2 = determine_game( game2 );
+        }
+        std::cout << " enter your sens\n" << clr::green << "  > " << clr::reset;
+        std::cin >> input_sens;
+
+        conv = input_sens * input_game1;
+        std::cout << " " << input_sens << " on " << game1 << " = " << clr::green << ( conv / input_game2 ) << clr::reset << " on " << game2 << "\n ";
+        system( "pause" );
+        std::cout << "\n";
+    }
+
+    // game cm dpi -> sens
+    void sens() {
+        std::string game;
+        double input_game,
+               input_cm;
+        int input_dpi;
+
+        while ( input_game == 0 ) {
+            std::cout << " enter your desired game\n" << clr::green << "  > " << clr::reset;
+            std::cin >> game;
+            input_game = determine_game( game );
+        }
+
+        std::cout << " enter your cm\n" << clr::green << "  > " << clr::reset;
+        std::cin >> input_cm;
+        std::cout << " enter your dpi\n" << clr::green << "  > " << clr::reset;
+        std::cin >> input_dpi;
+
+        get_sens( input_game, input_cm, input_dpi );
+    }
+
+    // outputs list of supported games
+    void games() {
+        // sort the GAME_YAWS vector by yaw value
+        std::vector<GameYaw> sorted = GAME_YAWS;
+        std::sort( sorted.begin(), sorted.end(), []( const GameYaw &a, const GameYaw &b ) { return a.yaw < b.yaw; } );
+
+        // group games with the same yaw value
+        double current_yaw = -1;
+        bool first_iteration = true;
+        for ( const auto &gameYaw: sorted ) {
+            if ( gameYaw.yaw != current_yaw ) {
+                current_yaw = gameYaw.yaw;
+                char buffer[50];
+                auto result = std::to_chars( buffer, buffer + sizeof( buffer ), current_yaw, std::chars_format::general, 11 );
+                *result.ptr = '\0'; // add a null terminator
+                std::string num_str( buffer );
+                if ( num_str.length() < 8 ) {
+                    if ( !first_iteration ) std::cout << "\n";
+                    std::cout << "\t" << num_str << clr::green << "\t\t-> " << clr::reset;
+                }
+                else {
+                    if ( !first_iteration ) std::cout << "\n";
+                    std::cout << "\t" << num_str << clr::green << "\t-> " << clr::reset;
+                }
+                first_iteration = false;
+            }
+            else { std::cout << ", "; }
+            std::cout << gameYaw.game;
+        }
+        std::cout << "\n";
+        system( "pause" );
+    }
+
+    // outputs help text
+    void help() {
+        std::cout << "\tcm \t "     << clr::green << "->" << clr::reset << " outputs cm/360 based on sens and game\n" // game sens dpi -> cm/360
+                  << "\tconvert\t " << clr::green << "->" << clr::reset << " converts sens between games\n"           // game1 game2 sens -> newsens
+                  << "\tsens \t "   << clr::green << "->" << clr::reset << " outputs sens based on game and dpi\n"    // game cm dpi -> sens
+                  << "\tgames \t "  << clr::green << "->" << clr::reset << " lists supported games\n"                 // list of supported games
+                  << "\texit \t "   << clr::green << "->" << clr::reset << " closes the application\n\n ";            // closes the calc
+        system( "pause" );
+    }
 }
 
 // outputs welcome message
@@ -139,57 +187,14 @@ void welcome() {
               << clr::green << "  > " << clr::reset;
 }
 
-// outputs list of supported games
-void games() {
-    // sort the GAME_YAWS vector by yaw value
-    std::vector<GameYaw> sorted = GAME_YAWS;
-    std::sort(sorted.begin(), sorted.end(), []( const GameYaw& a, const GameYaw& b ) { return a.yaw < b.yaw;} );
-
-    // group games with the same yaw value
-    double current_yaw = -1;
-    bool first_iteration = true;
-    for ( const auto& gameYaw : sorted ) {
-        if ( gameYaw.yaw != current_yaw ) {
-            current_yaw = gameYaw.yaw;
-            char buffer[50];
-            auto result = std::to_chars( buffer, buffer + sizeof( buffer ), current_yaw, std::chars_format::general, 11 );
-            *result.ptr = '\0'; // add a null terminator
-            std::string num_str(buffer);
-            if (num_str.length() < 8) {
-                if ( !first_iteration ) std::cout << "\n";
-                std::cout << "\t" << num_str << clr::green << "\t\t-> " << clr::reset;
-            }
-            else {
-                if ( !first_iteration ) std::cout << "\n";
-                std::cout << "\t" << num_str << clr::green << "\t-> " << clr::reset;
-            }
-            first_iteration = false;
-        }
-        else std::cout << ", ";
-        std::cout << gameYaw.game;
-    }
-    std::cout << "\n";
-    system( "pause" );
-}
-
-// outputs help text
-void help() {
-    std::cout << "\tcm \t "     << clr::green << "->" << clr::reset << " outputs cm/360 based on sens and game\n" // game sens dpi -> cm/360
-              << "\tconvert\t " << clr::green << "->" << clr::reset << " converts sens between games\n"           // game1 game2 sens -> newsens
-              << "\tsens \t "   << clr::green << "->" << clr::reset << " outputs sens based on game and dpi\n"    // game cm dpi -> sens
-              << "\tgames \t "  << clr::green << "->" << clr::reset << " lists supported games\n"                 // list of supported games
-              << "\texit \t "   << clr::green << "->" << clr::reset << " closes the application\n\n ";            // closes the calc
-    system( "pause" );
-}
-
 // input
 // initial menu comparatives
-void menu( const std::string& input ) {
-    if ( input == "cm" )      cm();
-    if ( input == "convert" ) convert();
-    if ( input == "sens" )    sens();
-    if ( input == "games" )   games();
-    if ( input == "help" )    help();
+void menu( const std::string &input ) {
+    if ( input == "cm" )        sens_converter::cm();
+    if ( input == "convert" )   sens_converter::convert();
+    if ( input == "sens" )      sens_converter::sens();
+    if ( input == "games" )     sens_converter::games();
+    if ( input == "help" )      sens_converter::help();
 }
 
 // main func
